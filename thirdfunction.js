2b1d1f095b44243ca2075a493c15bcef80e721a4
vixdug <victoria.duggan@shopify.com>
2017-01-16 20:27:24 +0000

    more modular function

---
+++ thirdfunction.js
@@ -0,0 +1,27 @@
+var https = require('https');
+
+function getAndPrintHTML (options) {
+
+    https.get(options, function (response) {
+
+      response.setEncoding('utf8');
+      var buffer = "";
+
+      response.on('data', function (data) {
+        buffer += data;
+    });
+
+      response.on('end', function() {
+        console.log(buffer);
+
+      });
+    });
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML(requestOptions)
